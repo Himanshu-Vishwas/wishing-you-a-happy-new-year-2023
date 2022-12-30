e37ed101a82969f10397c12e7be2d1e805f44bc2
Himanshu-vishwas <himanshuvishwas58@gmail.com>
2022-12-30 14:41:33 +0530

    Updates

---
+++ precache-manifest.c26a9897e618a728dcfa5fecb2148f3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4825ad7461c70acbcc94139ed1736af",
+    "url": "/wishing-you-a-happy-new-year-2023/index.html"
+  },
+  {
+    "revision": "7f4aa1e41ddb3a0d0b8f",
+    "url": "/wishing-you-a-happy-new-year-2023/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b7016b5e426bb05124d4",
+    "url": "/wishing-you-a-happy-new-year-2023/static/js/2.c2f2a4bd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/wishing-you-a-happy-new-year-2023/static/js/2.c2f2a4bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f4aa1e41ddb3a0d0b8f",
+    "url": "/wishing-you-a-happy-new-year-2023/static/js/main.ba0cd197.chunk.js"
+  },
+  {
+    "revision": "84fa7edf4b665a629f78",
+    "url": "/wishing-you-a-happy-new-year-2023/static/js/runtime-main.0729bc24.js"
+  }
+]);
